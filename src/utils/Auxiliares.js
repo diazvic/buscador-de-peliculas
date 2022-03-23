36aafffbb03bf618c8f4a74d6d215955aff2f49c
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-03-23 13:05:57 -0300

    Creo variables auxiliares

---
+++ src/utils/Auxiliares.js
@@ -0,0 +1,4 @@
+export const urlBase = "https://api.themoviedb.org/3/";
+export const apiKey = "api_key=2d1d912928e5c447a9dcdeaf620cab9b";
+export const queryParamLenguaje = "&language=";
+export const queryParamPagina = "&page=";
