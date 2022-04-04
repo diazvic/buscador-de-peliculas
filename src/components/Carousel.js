082f6531b5190ac25281fd5a074ffd3adc6cafb6
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-04-04 00:25:10 -0300

    Agrego carrusel

---
+++ src/components/Carousel.js
@@ -0,0 +1,50 @@
+import useFetchPeliculas from "../hooks/useFetchPeliculas";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "../styles/_Carousel.scss";
+import TarjetaCarousel from "./TarjetaCarousel";
+const Carousel = ({ categoria }) => {
+	const objetoPeliculas = useFetchPeliculas(categoria);
+	console.log("carrusel", objetoPeliculas);
+
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoPlaySpeed: true,
+	};
+	return (
+		<div>
+			<Slider {...settings}>
+				{objetoPeliculas.peliculas.map((pelicula) => (
+					<div key={pelicula.id} className="contenedor-diapositivas">
+						<div
+							style={{
+								backgroundImage:
+									"url(" +
+									` https://image.tmdb.org/t/p/original/${pelicula.backdrop_path}` +
+									")",
+								backgroundRepeat: "no-repeat",
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								backgroundColor: "red",
+							}}
+						></div>
+						<TarjetaCarousel
+							titulo={pelicula.title}
+							informacion={pelicula.overview}
+							link={`/movie/${pelicula.id}`}
+						/>
+					</div>
+				))}
+			</Slider>
+		</div>
+	);
+};
+
+export default Carousel;
+
+// fetch con now_playing
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,22 @@
 import ListaPeliculas from "./ListaPeliculas";
+import Carousel from "./Carousel";
 import "../styles/_Home.scss";
 import "../styles/_ListaPeliculas.scss";
 const Home = () => {
 	return (
-		<section className="contenedor-peliculas">
-			<div className="div-lista-peliculas">
-				<ListaPeliculas titulo="Peliculas Populares" categoria="popular" />
-			</div>
-			<div className="div-lista-peliculas">
-				<ListaPeliculas
-					titulo="Peliculas Mejor Puntuadas"
-					categoria="top_rated"
-				/>
-			</div>
+		<section>
+			<Carousel categoria="now_playing" />
+			<main className="contenedor-peliculas">
+				<div className="div-lista-peliculas">
+					<ListaPeliculas titulo="Peliculas Populares" categoria="popular" />
+				</div>
+				<div className="div-lista-peliculas">
+					<ListaPeliculas
+						titulo="Peliculas Mejor Puntuadas"
+						categoria="top_rated"
+					/>
+				</div>
+			</main>
 		</section>
 	);
 };
