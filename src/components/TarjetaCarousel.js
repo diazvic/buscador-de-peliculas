a0bc8adee3eee2b37b2f7569545731cd6f365522
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-04-04 15:55:49 -0300

    Implemento carrusel

--- src/components/TarjetaCarousel.js
+++ src/components/TarjetaCarousel.js
@@ -2,7 +2,7 @@ import "../styles/_TarjetaCarousel.scss";
 import { Link } from "react-router-dom";
 const TarjetaCarousel = ({ titulo, informacion, link }) => {
 	return (
-		<article>
+		<article className="tarjeta-carrusel">
 			<h2>{titulo}</h2>
 			<p>{informacion}</p>
 			<Link to={link}>
