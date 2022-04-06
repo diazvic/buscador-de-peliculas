51b215db804ed6859b658666b5cecebea41885ba
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-04-06 19:57:35 -0300

    Agrego componente de busqueda

--- src/components/Buscar.js
+++ src/components/Buscar.js
@@ -1,46 +1,61 @@
 import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import useFetchBusqueda from "../hooks/useFetchBusqueda";
+import TarjetaPeliculas from "./TarjetaPeliculas";
 import "../styles/_Buscar.scss";
+import TextField from "@mui/material/TextField";
+
 const Buscar = () => {
 	const [busquedaParametros, setBusquedaParametros] = useSearchParams({
 		busqueda: "",
 	});
 	const [arrayPeliculas, setPeliculas] = useState([]);
 
-	useEffect(() => {
-		// setLoader(true);
-		fetch(
-			`https://api.themoviedb.org/3/${busquedaParametros.get(
-				"busqueda"
-			)}/company?api_key=2d1d912928e5c447a9dcdeaf620cab9b`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				setPeliculas(data.results);
-			});
-	}, []);
 	const handleChange = (e) => {
 		setBusquedaParametros({
 			busqueda: e.target.value,
 		});
-		// const objetoBusqueda = useFetchBusqueda(busqueda);
 	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	useEffect(() => {
+		if (busquedaParametros.get("busqueda") != "") {
+			fetch(
+				`https://api.themoviedb.org/3/search/movie?api_key=2d1d912928e5c447a9dcdeaf620cab9b&language=es&query=${busquedaParametros.get(
+					"busqueda"
+				)}`
+			)
+				.then((res) => res.json())
+				.then((data) => {
+					setPeliculas(data.results);
+				});
+		}
+	}, [busquedaParametros]);
+
 	return (
-		<form>
-			<label>
-				<input
-					type="text"
-					onChange={handleChange}
-					value={busquedaParametros.get("busqueda")}
-				></input>
-			</label>
-			<label>
-				<input type="submit"></input>
-			</label>
-		</form>
+		<>
+			<form onSubmit={handleSubmit}>
+				<label>
+					<h1>Busca tu pelicula</h1>
+					<TextField
+						sx={{ width: "100%", p: 1 }}
+						onChange={handleChange}
+						value={busquedaParametros.get("busqueda")}
+					></TextField>
+				</label>
+			</form>
+			<div className="contenedor-tarjetas-peliculas">
+				{arrayPeliculas.map((pelicula) => (
+					<TarjetaPeliculas
+						key={pelicula.id}
+						titulo={pelicula.title}
+						imagen={`https://image.tmdb.org/t/p/w200/${pelicula.poster_path}`}
+						link={`/movie/${pelicula.id}`}
+					/>
+				))}
+			</div>
+		</>
 	);
 };
 
 export default Buscar;
-// clave, valor
