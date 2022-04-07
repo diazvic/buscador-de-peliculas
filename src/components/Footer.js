cb6615753edaed80427a6d79c2b1bacb0241a2ea
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-04-07 12:04:32 -0300

    Agrego footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,30 @@
+import "../styles/_Footer.scss";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
 const Footer = () => {
-	return <div>footer</div>;
+	return (
+		<footer className="footer">
+			<p>Hecho por Victoria Diaz</p>
+			<ul className="footer-iconos">
+				<li>
+					<a href="https://github.com/diazvic">
+						<FaGithub className="footer-iconos" />
+					</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/diazvic">
+						<FaLinkedinIn className="footer-iconos" />
+					</a>
+				</li>
+				<li>
+					<a href="mailto: vico.9467@gmail.com">
+						<MdOutlineMail className="footer-iconos" />
+					</a>
+				</li>
+			</ul>
+		</footer>
+	);
 };
 
 export default Footer;
